ca6631c6a22f8177d4fc20c51090d77ab905091a
eiu165 <eiu165@gmail.com>
2012-02-20 20:42:05 -0600

    sqlitescreen working
    its in better shape now

--- Multi/Screens/Demos/SqliteScreen.cs
+++ Multi/Screens/Demos/SqliteScreen.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
+using System.Text;
 using System.Drawing;
-
+using System.Data;
+using Mono.Data.Sqlite;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -10,6 +13,7 @@ namespace Multi
 	{
 		public SqliteScreen () : base ("SqliteScreen", null)
 		{
+			this.Title = "Sqlite"; 
 		}
 		
 		public override void DidReceiveMemoryWarning ()
@@ -22,9 +26,95 @@ namespace Multi
 		
 		public override void ViewDidLoad ()
 		{
-			base.ViewDidLoad ();
 			
-			// Perform any additional setup after loading the view, typically from a nib.
+			this.btnSave.TouchUpInside += (sender, e) => { 
+				this.WritePersonInDb (this.tbFirst.Text, this.tbLast.Text);
+				this.tfOutput.Text = ReadFromDb();
+				this.View.EndEditing (true);
+			};
+			
+			this.btnClear.TouchUpInside += (sender, e) => { 
+				var documents = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+				string db = Path.Combine (documents, "mydb.db3");
+				bool exists = File.Exists (db); 
+				if (exists) {
+					File.Delete (db);
+				}
+				this.tfOutput.Text = ReadFromDb();
+			};
+			
+			base.ViewDidLoad (); 
+		}
+		
+		public   void WritePersonInDb (string firstName, string email)
+		{
+			var connection = GetConnection ();
+			var commands = new [] {"INSERT into people (PersonID, FirstName, LastName) VALUES ( 4, '" + firstName + "', '" + email + "')",};  
+			foreach (var cmd in commands) {
+				using (var c = connection.CreateCommand()) {
+					c.CommandText = cmd;
+					c.CommandType = CommandType.Text;
+					connection.Open ();
+					c.ExecuteNonQuery ();
+					connection.Close ();
+					Console.Error.WriteLine ("about to run    " + cmd.ToString ());
+				}
+			} 
+		}
+		
+		public static string ReadFromDb ()
+		{
+			var sb = new StringBuilder ();
+			var connection = GetConnection ();
+			using (var cmd = connection.CreateCommand ()) {
+				connection.Open ();
+				cmd.CommandText = "SELECT * FROM People";
+				using (var reader = cmd.ExecuteReader ()) {
+					while (reader.Read ()) {
+						Console.Error.Write ("(Row "); 
+						for (int i = 1; i < reader.FieldCount; ++i) { 
+							//Write (reader, i);
+							var name = reader.GetName (i);
+							var val = reader [i];
+							Console.Error.Write ("[{0}:'{1}']", name, val); 
+							sb.AppendFormat ("{0} ", val);
+						}
+						Console.Error.WriteLine (")");
+						sb.Append (")  -\n");
+					}
+				}
+				connection.Close ();
+			}
+			return sb.ToString ();
+		}
+		
+		public static SqliteConnection GetConnection ()
+		{
+			var documents = Environment.GetFolderPath (
+                Environment.SpecialFolder.Personal);
+			string db = Path.Combine (documents, "mydb.db3");
+			bool exists = File.Exists (db);
+			//	exists = false;
+			
+			if (!exists)
+				SqliteConnection.CreateFile (db);
+			var conn = new SqliteConnection ("Data Source=" + db);
+			if (!exists) {
+				var commands = new[] {
+		            "CREATE TABLE People (PersonID INTEGER NOT NULL, FirstName ntext, LastName ntext)",
+		            "INSERT INTO People (PersonID, FirstName, LastName) VALUES (1, 'Homer', 'Simpson')", 
+		        };
+				foreach (var cmd in commands){
+					using (var c = conn.CreateCommand()) {
+						c.CommandText = cmd;
+						c.CommandType = CommandType.Text;
+						conn.Open ();
+						c.ExecuteNonQuery ();
+						conn.Close ();
+					}
+				}
+			}
+			return conn;
 		}
 		
 		public override void ViewDidUnload ()
--- Multi/Screens/Demos/SqliteScreen.designer.cs
+++ Multi/Screens/Demos/SqliteScreen.designer.cs
@@ -11,8 +11,47 @@ namespace Multi
 	[Register ("SqliteScreen")]
 	partial class SqliteScreen
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField tbFirst { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField tbLast { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnSave { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextView tfOutput { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnClear { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (tbFirst != null) {
+				tbFirst.Dispose ();
+				tbFirst = null;
+			}
+
+			if (tbLast != null) {
+				tbLast.Dispose ();
+				tbLast = null;
+			}
+
+			if (btnSave != null) {
+				btnSave.Dispose ();
+				btnSave = null;
+			}
+
+			if (tfOutput != null) {
+				tfOutput.Dispose ();
+				tfOutput = null;
+			}
+
+			if (btnClear != null) {
+				btnClear.Dispose ();
+				btnClear = null;
+			}
 		}
 	}
 }
